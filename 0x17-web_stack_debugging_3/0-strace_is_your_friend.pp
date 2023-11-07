1a83b2911d4e4f2ef0b226109a54e8e77e8fb1e5
Temiloluwa22 <113702411+Temiloluwa22@users.noreply.github.com>
2023-11-07 19:52:58 +0100

    Code

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Use strace to find out why Apache is returning a 500 error
+# automate it using Puppet 
+
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
